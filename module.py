c5846b6ef105c3a360e45c74e03854952fccf09d
Ala Ho <alaandho@gmail.com>
2021-07-15 02:05:46 -0400

    python day3

---
+++ module.py
@@ -0,0 +1,11 @@
+from math import pi
+
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+    
+
+def printArea(length, width):
+    print(length * width)
+    
+def printRad(radius):
+    print(2* pi * radius)
